ba9c5397b6bef58ab0cd6bdd85b2964e49c4d3a6
CodeIntelli <codeintelli2021@gmail.com>
2021-11-02 14:19:31 +0530

    path problem

--- TODOAPP/settings.py
+++ TODOAPP/settings.py
@@ -87,9 +87,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
